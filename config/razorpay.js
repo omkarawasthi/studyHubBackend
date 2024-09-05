48f203c3bf8b53de9f2498c656acd2136e80065c
Omkar <omkarawasthi67@gmail.com>
2024-09-05 12:03:20 +0530

    Sixteenth Commit

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,5 +1,6 @@
-const Razorpay = require("razorpay");
 
+const Razorpay = require("razorpay");
+require('dotenv').config();
 
 exports.instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
