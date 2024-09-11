f584e9c96065d110941976f0f56a214676216707
Omkar <omkarawasthi67@gmail.com>
2024-09-11 12:14:17 +0530

    Ninteenth Commit

--- controllers/Profile.js
+++ controllers/Profile.js
@@ -4,27 +4,58 @@ const { uploadImageToCloudinary } = require("../utils/imageUploader");
 // Method for updating a profile
 exports.updateProfile = async (req, res) => {
 	try {
-		const { dateOfBirth = "", about = "", contactNumber } = req.body;
+		const { firstName, lastName, gender, dateOfBirth = "", about = "", contactNumber } = req.body;
 		const id = req.user.id;
+
+		console.log(firstName)
+		console.log(lastName)
+		// console.log(dateOfBirth);
 
 		// Find the profile by id
 		const userDetails = await User.findById(id);
-		const profile = await Profile.findById(userDetails.additionalDetails);
+		userDetails.firstName = firstName;
+		userDetails.lastName = lastName;
+
+		await userDetails.save();
+		// console.log("userDetails :", userDetails.additionalDetails.toString());
+		const profile = await Profile.findById(userDetails.additionalDetails.toString());
+		// console.log("Profile: ", profile)
+
+		if (!profile) {
+			const newProfile = new Profile({
+				_id: userDetails.additionalDetails,
+				dateOfBirth,
+				about,
+				contactNumber,
+				gender,
+			});
+			await newProfile.save();
+			return res.json({
+				success: true,
+				message: "Profile created successfully",
+				profile,
+			});
+		}
 
 		// Update the profile fields
 		profile.dateOfBirth = dateOfBirth;
 		profile.about = about;
 		profile.contactNumber = contactNumber;
+		profile.gender = gender;
 
 		// Save the updated profile
 		await profile.save();
+		const updatedUserDetails = await User.findById(id)
+			.populate("additionalDetails")
+			.exec()
 
 		return res.json({
 			success: true,
 			message: "Profile updated successfully",
-			profile,
-		});
-	} catch (error) {
+			updatedUserDetails,
+		})
+	}
+	catch (error) {
 		console.log(error);
 		return res.status(500).json({
 			success: false,
@@ -42,7 +73,7 @@ exports.deleteAccount = async (req, res) => {
 		// console.log(job);
 		console.log("Printing ID: ", req.user.id);
 		const id = req.user.id;
-		
+
 		const user = await User.findById({ _id: id });
 		if (!user) {
 			return res.status(404).json({
@@ -88,63 +119,63 @@ exports.getAllUserDetails = async (req, res) => {
 };
 
 exports.updateDisplayPicture = async (req, res) => {
-    try {
-      const displayPicture = req.files.displayPicture
-      const userId = req.user.id
-      const image = await uploadImageToCloudinary(
-        displayPicture,
-        process.env.FOLDER_NAME,
-        1000,
-        1000
-      )
-      console.log(image)
-      const updatedProfile = await User.findByIdAndUpdate(
-        { _id: userId },
-        { image: image.secure_url },
-        { new: true }
-      )
-      res.send({
-        success: true,
-        message: `Image Updated successfully`,
-        data: updatedProfile,
-      })
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        message: error.message,
-      })
-    }
+	try {
+		const displayPicture = req.files.displayPicture
+		const userId = req.user.id
+		const image = await uploadImageToCloudinary(
+			displayPicture,
+			process.env.FOLDER_NAME,
+			1000,
+			1000
+		)
+		console.log(image)
+		const updatedProfile = await User.findByIdAndUpdate(
+			{ _id: userId },
+			{ image: image.secure_url },
+			{ new: true }
+		)
+		res.send({
+			success: true,
+			message: `Image Updated successfully`,
+			data: updatedProfile,
+		})
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error.message,
+		})
+	}
 };
-  
+
 exports.getEnrolledCourses = async (req, res) => {
-    try {
-      const userId = req.user.id
-      const userDetails = await User.findOne({ _id: userId })
-      .populate({
-        path: "courses", // Populating courses array
-        populate: {
-          path: "courseContent", // Populating sections inside courseContent
-          populate: {
-            path: "subSection", // Populating subsections inside sections
-            model: "SubSection", // Referencing the SubSection model
-          },
-        },
-      })
-      .exec();
-      if (!userDetails) {
-        return res.status(400).json({
-          success: false,
-          message: `Could not find user with id: ${userDetails}`,
-        })
-      }
-      return res.status(200).json({
-        success: true,
-        data: userDetails.courses,
-      })
-    } catch (error) {
-      return res.status(500).json({
-        success: false,
-        message: error.message,
-      })
-    }
+	try {
+		const userId = req.user.id
+		const userDetails = await User.findOne({ _id: userId })
+			.populate({
+				path: "courses", // Populating courses array
+				populate: {
+					path: "courseContent", // Populating sections inside courseContent
+					populate: {
+						path: "subSection", // Populating subsections inside sections
+						model: "SubSection", // Referencing the SubSection model
+					},
+				},
+			})
+			.exec();
+		if (!userDetails) {
+			return res.status(400).json({
+				success: false,
+				message: `Could not find user with id: ${userDetails}`,
+			})
+		}
+		return res.status(200).json({
+			success: true,
+			data: userDetails.courses,
+		})
+	} catch (error) {
+		return res.status(500).json({
+			success: false,
+			message: error.message,
+		})
+	}
 };
